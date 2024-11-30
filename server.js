caf2f4eaaacddd9e3b08098605e6891e15f2061f
Rodion Shapovalov <rodion.shap@gmail.com>
2024-11-30 22:28:36 +0100

    sd

--- server.js
+++ server.js
@@ -5,18 +5,27 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
+const __dirname = process.cwd();
+//_______________________________________________________
+app.use(express.static(__dirname, {dotfiles: 'allow'})); //
+app.use(express.static(path.join(__dirname, '.well-known', 'acme-challenge')));
 
+// Serve ACME challenge files
+app.use(
+	'/.well-known/acme-challenge/*',
+	express.static(path.join(__dirname, '.well-known', 'acme-challenge'))
+);
 // Weiterleitung von HTTP auf HTTPS
-// app.all('*', (req, res) => {
-// 	const httpsUrl = 'https://' + req.headers.host + req.url;
-// 	res.redirect(301, httpsUrl); // Permanent Redirect
-// });
+app.all('*', (req, res) => {
+	const httpsUrl = 'https://' + req.headers.host + req.url;
+	res.redirect(301, httpsUrl); // Permanent Redirect
+});
 
 // Erstelle einen HTTP-Server (Port 80)
-// const httpServer = http.createServer(app);
-// httpServer.listen(8080, () => {
-// 	console.log('HTTP-Server läuft auf Port 80, Weiterleitung aktiv.');
-// });
+const httpServer = http.createServer(app);
+httpServer.listen(8080, () => {
+	console.log('HTTP-Server läuft auf Port 80, Weiterleitung aktiv.');
+});
 
 const KEY_PATH = path.join(
 	process.cwd(),
